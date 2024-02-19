9177e66f50a80b7be7347e2b020d25c1db024cad
jmmes <jmmes@163.com>
2024-02-19 20:59:01 +0800

    Update example-custom-script-1.sh

--- example-custom-script-1.sh
+++ example-custom-script-1.sh
@@ -16,10 +16,10 @@
 # This is followed by some editing examples.
 # # Clear the feeds.conf.default and append the feed sources you need one by one:
 #cat /dev/null > a.txt
-echo 'src-git-full packages https://git.openwrt.org/feed/packages.git;openwrt-23.05' >> feeds.conf.default
-echo 'src-git-full luci https://git.openwrt.org/project/luci.git;openwrt-23.05' >> feeds.conf.default
-echo 'src-git-full routing https://git.openwrt.org/feed/routing.git;openwrt-23.05' >> feeds.conf.default
-echo 'src-git-full telephony https://git.openwrt.org/feed/telephony.git;openwrt-23.05' >> feeds.conf.default
+#echo 'src-git-full packages https://git.openwrt.org/feed/packages.git;openwrt-23.05' >> feeds.conf.default
+#echo 'src-git-full luci https://git.openwrt.org/project/luci.git;openwrt-23.05' >> feeds.conf.default
+#echo 'src-git-full routing https://git.openwrt.org/feed/routing.git;openwrt-23.05' >> feeds.conf.default
+#echo 'src-git-full telephony https://git.openwrt.org/feed/telephony.git;openwrt-23.05' >> feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
 echo 'src-git PWpackages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
 # # Replace a feed source with what you want:
